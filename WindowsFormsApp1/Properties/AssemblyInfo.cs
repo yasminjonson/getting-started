f16c58d82d5bd3b146a827091b4bbea9569dbd01
yasminjohnson <yasminj2019@gmail.com>
2017-09-14 10:59:02 -0400

    Revert "fixed project"
    This reverts commit 73ead8d9cc4fbc2798968277872250b1bb7a2b02.

--- WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FavoriteFood")]
+[assembly: AssemblyTitle("WindowsFormsApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FavoriteFood")]
+[assembly: AssemblyProduct("WindowsFormsApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("128ba9d9-d32b-4cfe-9fe1-0c11e39661bc")]
+[assembly: Guid("d5a4a404-715e-4647-8801-78333f5d2eaf")]
 
 // Version information for an assembly consists of the following four values:
 //
