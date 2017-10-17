a790f0e3ebf57b3328c2675f2fbf89a1c78bf6e3
yasminjonson <yasminj2019@gmail.com>
2017-10-17 11:27:25 -0400

    fizzbuzz and some other bullshite

---
+++ GettingStarted/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+
+            Enumerable.Range(1, 100).ToList()
+                .ForEach(x =>
+                {
+                    Console.WriteLine("{0}{1}{2}",
+                        x % 3 == 0 ? "Fizz" : "",
+                        x % 5 == 0 ? "Buzz" : "",
+                        x % 3 != 0 && x % 5 != 0 ? x.ToString() : "");
+                });
+        }
+    }
+}
--- GettingStarted/Max2/Program.cs
+++ GettingStarted/Max2/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GettingStarted
+namespace Max2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // specify data structure
+            int[] numbers = new int[5000];
+
 
         }
     }
