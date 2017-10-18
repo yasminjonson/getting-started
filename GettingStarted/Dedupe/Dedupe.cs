cef7635958d4555c3d751df4b9bd99a006ed4740
yasmin johnson <yasminj2019@gmail.com>
2017-10-18 12:39:10 -0400

    ;iauehrg

---
+++ GettingStarted/Dedupe/Dedupe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dedupe
+{
+    class Dedupe
+    {
+        static void Main(string[] args)
+        {
+            // does the first part (array of 5000)
+            int min = 0;
+            int max = 50;
+
+            int[] numbers = new int[5000];
+
+            Random r = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            { numbers[i] = r.Next(min, max); }
+
+            // sort from least to greatest
+            Array.Sort(numbers);
+
+        }
+    }
+}
---
+++ MinMax/MinMax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class MinMax
+    {
+        static void Main(string[] args)
+        {
+
+            int min = 1;
+            int max = 50;
+
+            int[] numbers = new int[50];
+
+            Random r = new Random();
+            for( int i = 0; i < numbers.Length; i++ )
+            { numbers[i] = r.Next (min, max ); }
+
+            for (int y = 0; y < 5; y++)
+            {
+                String row = "";
+                for (int x = 0; x < 10; x++)
+                {
+                    String c = numbers[ y * 10 + x ] + ", ";
+                    row += c.PadRight(5,' ');
+                }
+                Console.WriteLine( row + "\n" );  
+            }
+
+            int maxx = numbers.Max();
+            Console.WriteLine("The largest number in the array is: " + maxx);
+
+            int minn = numbers.Min();
+            Console.WriteLine("The smallest number in the array is: " + minn);
+
+
+        }
+    }
+}
