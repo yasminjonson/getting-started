a7784b0c03d698d55baeff7d03e6030574f3dfdb
yasminjonson <yasminj2019@gmail.com>
2017-10-19 12:27:15 -0400

    ugrhoiugerohi'

--- GettingStarted/Dedupe/Dedupe.cs
+++ GettingStarted/Dedupe/Dedupe.cs
@@ -24,6 +24,7 @@ namespace Dedupe
             Array.Sort(numbers);
 
             Console.WriteLine(numbers);
+            
 
         }
     }
---
+++ UniqueDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>();
+
+            for (int i = 1; i <= 54; i++)
+                Numbers.Add(i);
+
+            IEnumberable<int> AcceptedNumbers = Numbers.Where(N => N.ToString().Distinct().Count() == N.ToString().Length);
+            
+            foreach (int AcceptedNumber in AcceptedNumbers)
+                Console.WriteLine(AcceptedNumber);
+
+        }
+    }
+}
