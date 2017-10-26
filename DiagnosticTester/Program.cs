64044213c3149ba6d4b12bdb574f208cb10542da
yasmin johnson <yasminj2019@gmail.com>
2017-10-26 12:13:20 -0400

    bullshite

---
+++ DiagnosticTester/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosticTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        public String question;
+        public String[] answers;
+        public int correctAnswer;
+
+        public int category;
+        public int level;
+
+            public Question(int category, int level, String q, String[] a, int correct)
+            {
+              this.category = category;
+              this.question = q;
+              this.level = level;
+                
+              this.answers = a;
+              this.correctAnswer = correct;
+            }
+
+        private List<Question> questions = new List<Question>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
